1b3a0813387489013bd8a6da51df95dff92b901f
yatharthsanghvi <141222209+yatharthsanghvi@users.noreply.github.com>
2024-08-20 18:14:14 +0530

    Update app.py

--- fileshare/app.py
+++ fileshare/app.py
@@ -136,4 +136,5 @@ def internal_error(error):
     return render_template('error.html', error_message="Internal server error"), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
